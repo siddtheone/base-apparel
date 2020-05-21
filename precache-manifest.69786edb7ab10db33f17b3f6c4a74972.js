31fb0270b16fe1e326bbd11f77c8ac9a8f282230
siddtheone <siddharthl@cybage.com>
2020-05-21 17:52:34 +0530

    Updates

---
+++ precache-manifest.69786edb7ab10db33f17b3f6c4a74972.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a8f42facc853323aff28313e648efd",
+    "url": "/base-apparel/index.html"
+  },
+  {
+    "revision": "4f411bc8bcb944f2bbae",
+    "url": "/base-apparel/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "94d7fb76052531596d74",
+    "url": "/base-apparel/static/js/2.db197279.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/base-apparel/static/js/2.db197279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f411bc8bcb944f2bbae",
+    "url": "/base-apparel/static/js/main.c909c865.chunk.js"
+  },
+  {
+    "revision": "d74ae62a37264ca14f1f",
+    "url": "/base-apparel/static/js/runtime-main.49211b21.js"
+  },
+  {
+    "revision": "9a99295173be6c0f6dbc1e173d75ce48",
+    "url": "/base-apparel/static/media/bg-pattern-desktop.9a992951.svg"
+  },
+  {
+    "revision": "003b0c9744b45dea37e19234916edb43",
+    "url": "/base-apparel/static/media/hero-desktop.003b0c97.jpg"
+  },
+  {
+    "revision": "1d55799afb7254685302a38efea12fd1",
+    "url": "/base-apparel/static/media/hero-mobile.1d55799a.jpg"
+  },
+  {
+    "revision": "69950c2de382a8d4924260343e516fad",
+    "url": "/base-apparel/static/media/icon-arrow.69950c2d.svg"
+  },
+  {
+    "revision": "337809979308399afb36fa5f0e3fbcdf",
+    "url": "/base-apparel/static/media/icon-error.33780997.svg"
+  },
+  {
+    "revision": "877543ce69f4ed6f834f26e7dc4ca930",
+    "url": "/base-apparel/static/media/logo.877543ce.svg"
+  }
+]);
